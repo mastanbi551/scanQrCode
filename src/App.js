2334c2bf65533d208939065acd8ab2526cb8746e
mastanbi551 <mastanbi551sk@gmail.com>
2023-07-19 13:18:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import QRCode from "qrcode.react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ marginTop: 200, display: "flex",flexDirection: "row" }}>
+         <div>
+            <QRCode
+               value="https://qrcodeapp-975af.web.app/"style={{ marginRight: 50 }}/>
+            <p>For Invitation Scan QR Code</p>
+         </div>
+        </div>
       </header>
     </div>
   );
